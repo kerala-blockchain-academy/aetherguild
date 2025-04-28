716895e38cb2b16f25122ef6f8f71203bee1ae57
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-04-28 21:28:40 +0530

    🧪 | start testing for druid
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

---
+++ druid/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func TestDruid(t *testing.T) {
+	flag := false
+	stack := makeDruid(&flag, &flag)
+	defer stack.Close()
+
+	if err := stack.Start(); err != nil {
+		t.Fatalf("Error starting protocol stack: %v", err)
+	}
+
+	rpcClient := stack.Attach()
+	ethClient := ethclient.NewClient(rpcClient)
+
+	var version string
+	if err := ethClient.Client().CallContext(context.Background(), &version, "web3_clientVersion"); err != nil {
+		t.Fatalf("Failed to fetch client version: %v", err)
+	}
+
+	vw := fmt.Sprintf("%s/%s-%s/%s", strings.TrimSuffix(filepath.Base(os.Args[0]), ".test"), runtime.GOOS, runtime.GOARCH, runtime.Version())
+	if version != vw {
+		t.Fatalf("Wrong client version: %s, expected: %s", version, vw)
+	}
+}
